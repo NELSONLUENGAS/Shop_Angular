a705a6ef7be247f2ae18f291b5a03532aed77f6b
NELSON ANDRES LUENGAS SILVA <90053867+NELSONLUENGAS@users.noreply.github.com>
2022-09-20 18:56:22 -0500

    generate bandle deploy

--- Frontend/src/environments/environment.prod.ts
+++ Frontend/src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  apiURL: 'http://localhost:3001',
+  STRIPE_PUBLIC_KEY: 'pk_test_51Lige1Cp3tieVWcOXSTo9ukUb0oUp80RUk2RnB90DCUWUxwnJFHqJZXzUJzVqWi86tO0LLSI1iwICU2EVwHXYekF00FZ5Wd4x4',
+  STRIPE_SECRET_KEY: 'sk_test_51Lige1Cp3tieVWcOhRcUhoMZU71rkblZpNHzPhUPeYuhkEbCbwnYuuIqTgfGK7kKFhuialfkmcwvRmCuoZyOfuF500XcBEUojL'
 };
